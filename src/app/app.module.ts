6c6616d7ab64293c8ed5a9979d16f4822d2f84dc
cqlzx <bupt11lzx@163.com>
2017-09-14 23:41:56 -0700

    add search-bar and results-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { SearchResultComponent } from './components/search-result/search-result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBarComponent,
+    SearchResultComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/models/movie.model.ts
@@ -0,0 +1,6 @@
+export class Movie {
+  id: number;
+  name: string;
+  year: number;
+  subtitle: string;
+}
